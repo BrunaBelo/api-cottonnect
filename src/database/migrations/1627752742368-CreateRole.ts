60be00bdc2f6d231015dc704d508acd910373faa
BrunaBelo <anabruna28@gmail.com>
2021-07-31 14:33:20 -0300

    Add config path migration

---
+++ src/database/migrations/1627752742368-CreateRole.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateRole1627752742368 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
