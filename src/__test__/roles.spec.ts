3ea1e4d88d96e0b30aac8982942cb847acb3b05c
BrunaBelo <anabruna28@gmail.com>
2021-08-01 17:12:32 -0300

    Add negative test to role

--- src/__test__/roles.spec.ts
+++ src/__test__/roles.spec.ts
@@ -14,4 +14,16 @@ describe("Create Role", () => {
     const newRole = await roleUseCase.createRole("Ana Bruna");
     expect(newRole.name).toBe("Ana Bruna");
   });
+
+  it("Shoud not create new role", async () => {
+    let newRole = await roleUseCase.createRole("Ana Bruna");
+    let error_message = "";
+    try {
+      newRole = await roleUseCase.createRole("Ana Bruna");
+    } catch (error) {
+      error_message = error.message;
+    }
+
+    expect(error_message).toBe(`O nome ${newRole.name} já está em uso`);
+  });
 });
