8aee33abf10cb6fbdff2ae0c2d9093417626828e
BrunaBelo <anabruna28@gmail.com>
2021-08-01 17:15:47 -0300

    Create migrate Bidding

---
+++ src/database/migrations/1627842378164-CreateBidding.ts
@@ -0,0 +1,62 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateBidding1627842378164 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "biddings",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "bid_amount",
+            type: "integer",
+          },
+          {
+            name: "winner",
+            type: "boolean",
+            default: false,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "user_id",
+            type: "uuid",
+          },
+          {
+            name: "aution_id",
+            type: "uuid",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "fk_user",
+            columnNames: ["user_id"],
+            referencedColumnNames: ["id"],
+            referencedTableName: "users",
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+          {
+            name: "fk_aution_id",
+            columnNames: ["aution_id"],
+            referencedColumnNames: ["id"],
+            referencedTableName: "autions",
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("biddings");
+  }
+}
