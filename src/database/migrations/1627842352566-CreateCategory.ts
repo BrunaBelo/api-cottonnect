c63c432c0f5cd8f0b634e9dc5299ec8d2be9fee9
BrunaBelo <anabruna28@gmail.com>
2021-08-01 17:14:48 -0300

    Create migrate Category

---
+++ src/database/migrations/1627842352566-CreateCategory.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCategory1627842352566 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "categories",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("categories");
+  }
+}
