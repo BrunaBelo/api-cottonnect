34dbea0cc444dc465a34b183718847a324c53a2f
BrunaBelo <anabruna28@gmail.com>
2021-08-06 22:05:20 -0300

    Add State repository

---
+++ src/repository/state.ts
@@ -0,0 +1,27 @@
+import { getRepository, Repository } from "typeorm";
+
+import { State } from "../model/state";
+
+class StateRepository {
+  private repository: Repository<State>;
+
+  constructor() {
+    this.repository = getRepository(State);
+  }
+
+  async create(state: State): Promise<State> {
+    const new_state = this.repository.create(state);
+    await this.repository.save(new_state);
+    return new_state;
+  }
+
+  async update(id: string, state: State): Promise<State> {
+    throw new Error("Method not implemented.");
+  }
+
+  async delete(id: string): Promise<State> {
+    throw new Error("Method not implemented.");
+  }
+}
+
+export { StateRepository };
