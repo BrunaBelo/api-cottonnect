3d983d5092d9a12dae213c9b5dfbd76a45177f7b
BrunaBelo <anabruna28@gmail.com>
2021-08-06 22:23:19 -0300

    Remove IRepository

--- src/irepositories/IRolesRepository.ts
+++
@@ -1,8 +0,0 @@
-import { Role } from "../model/Role";
-
-interface IRolesRepository {
-  create(name: string): Promise<Role>;
-  findByName(name: string): Promise<Role>;
-}
-
-export { IRolesRepository };
