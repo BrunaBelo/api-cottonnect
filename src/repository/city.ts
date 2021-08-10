5be939a1613b1e2994a622e91e19d50d43aa09d6
BrunaBelo <anabruna28@gmail.com>
2021-08-10 20:30:02 -0300

    Add city repository

---
+++ src/repository/city.ts
@@ -0,0 +1,28 @@
+import { getRepository, Repository } from "typeorm";
+import { City } from "../model/city";
+
+import { State } from "../model/state";
+
+class CityRepository {
+    private repository: Repository<City>;
+
+    constructor() {
+        this.repository = getRepository(City);
+    }
+
+    async create(city: City): Promise<City> {
+        const newCity = this.repository.create(city);
+        await this.repository.save(newCity);
+        return newCity;
+    }
+
+    async update(id: string, state: City): Promise<City> {
+        throw new Error("Method not implemented.");
+    }
+
+    async delete(id: string): Promise<City> {
+        throw new Error("Method not implemented.");
+    }
+}
+
+export { CityRepository };
