b383dbf7175103e82a62550ceb93eb456bc9ae88
BrunaBelo <anabruna28@gmail.com>
2021-08-12 20:03:08 -0300

    Add role factory

---
+++ src/__test__/factories/roleFactory.ts
@@ -0,0 +1,11 @@
+import { Role } from "../../model/role";
+
+export const roleAdmin: Role = {
+    id: "0bffa21b-2d1b-4d19-94c6-33dc388c3158",
+    name: "Admin",
+}
+
+export const roleUser: Role = {
+    id: "e2e68eae-cfcb-4693-8167-98258355f26e",
+    name: "User",
+}
