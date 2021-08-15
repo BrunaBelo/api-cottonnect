bcab9e5c87cd9f10de18166aec16e345b298a1d0
BrunaBelo <anabruna28@gmail.com>
2021-08-15 17:24:48 -0300

    Add user factory

---
+++ src/__test__/factories/auctionFactory.ts
@@ -0,0 +1,9 @@
+import { Auction } from "../../model/auction";
+
+export const auction01: Auction = {
+    id: "c4721895-d4a8-4162-9950-192fbcc98ad0",
+    name: "Auction 01",
+    closing_data: new Date(),
+    donation_object_id: "",
+    user_id: ""
+}
