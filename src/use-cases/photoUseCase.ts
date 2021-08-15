0979a40ed6dff9f6cd0811af8f64a6f8e7a4fa71
BrunaBelo <anabruna28@gmail.com>
2021-08-15 14:42:45 -0300

    Add Photo use case

---
+++ src/use-cases/photoUseCase.ts
@@ -0,0 +1,24 @@
+import { Photo } from "../model/photo";
+import { PhotoRepository } from "../repository/photoRepositoy";
+
+class PhotoUseCase {
+    private repository: PhotoRepository;
+
+    constructor() {
+        this.repository = new PhotoRepository();
+    }
+
+    async create(photo: Photo): Promise<Photo> {
+        const newPhoto = await this.repository.create(photo);
+        return newPhoto;
+    }
+
+    async findById(id: string): Promise<Photo> {
+        throw new Error("Method not implemented.");
+    }
+
+    async findByDonation(idDonation: string): Promise<Photo> {
+        throw new Error("Method not implemented.");
+    }
+}
+export { PhotoUseCase };
