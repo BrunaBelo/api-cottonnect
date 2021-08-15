7c937c9840a47bcc07fa4244bf2b7a40b5f3e771
BrunaBelo <anabruna28@gmail.com>
2021-08-15 15:42:36 -0300

    Add category controller

---
+++ src/controller/categoryController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+import { Category } from "../model/category";
+import { CategoryUseCase } from "../use-cases/categoryUseCase";
+
+class CategoryController {
+    async create(request: Request, response: Response): Promise<Response> {
+        const { name }: Category = request.body;
+
+        const CategoryUserCase = new CategoryUseCase();
+
+        const newCategory = await CategoryUserCase.create({ name });
+        return response.status(201).json(newCategory);
+    }
+}
+export { CategoryController };
