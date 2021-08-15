e3c3b65dd7b11d477338c7d1d4e0b7e3748faa48
BrunaBelo <anabruna28@gmail.com>
2021-08-15 14:02:01 -0300

    Add Donation Object factory

---
+++ src/__test__/factories/donationObjectFactory.ts
@@ -0,0 +1,8 @@
+import { getRepository } from "typeorm";
+import { DonationObject } from "../../model/donationObject";
+
+export const donation01: DonationObject = {
+    id: "b27554ec-2df6-4d0b-a3ac-aa2ec9d900fe",
+    title: "Mouse",
+    description: "Mouse Gamer Redragon Cobra, 10000DPI, Chroma, Preto - M711, perfeito estado"
+}
