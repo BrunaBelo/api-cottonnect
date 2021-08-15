cbac865f28d97ea4d4684b5531dd815d09c3c90d
BrunaBelo <anabruna28@gmail.com>
2021-08-15 14:57:13 -0300

    Add photo factory

---
+++ src/__test__/factories/photoFactory.ts
@@ -0,0 +1,9 @@
+import { Photo } from "../../model/photo";
+
+export const photo01: Photo = {
+    id: "729b3cc8-1088-4f88-a08b-5a40ec913ce4",
+    name: "photo01",
+    type: ".png",
+    uri: "/upload/photo01.png",
+    donationObjectId: "b27554ec-2df6-4d0b-a3ac-aa2ec9d900fe"
+}
