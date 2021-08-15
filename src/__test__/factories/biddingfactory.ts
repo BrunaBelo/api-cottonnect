0cab47fdbdbc86a98eb22d5921551aea25510409
BrunaBelo <anabruna28@gmail.com>
2021-08-15 18:07:17 -0300

    Add bidding factory

---
+++ src/__test__/factories/biddingfactory.ts
@@ -0,0 +1,8 @@
+import { Bidding } from "../../model/bidding";
+
+export const bidding01: Bidding = {
+    id: "44cd5754-5789-4c56-a2a1-64d4b4de8894",
+    bid_amount: 1,
+    auction_id: "",
+    user_id: "",
+}
