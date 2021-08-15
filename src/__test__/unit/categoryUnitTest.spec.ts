eea9d46c4813eb3f2292b0e5f3e2207fcf6b3977
BrunaBelo <anabruna28@gmail.com>
2021-08-15 15:47:35 -0300

    Add category tests

---
+++ src/__test__/unit/categoryUnitTest.spec.ts
@@ -0,0 +1,27 @@
+import "../../database/connection";
+import connection from "../../database/connection";
+import { CategoryUseCase } from "../../use-cases/categoryUseCase";
+import { category01 } from "../factories/categoryFactory";
+
+let category = category01
+let categoryUseCase: CategoryUseCase;
+
+describe("Create Category", () => {
+    beforeAll(async () => {
+        await connection.create();
+    });
+
+    afterAll(async () => {
+        await connection.close();
+    });
+
+    beforeEach(async () => {
+        await connection.clear();
+        categoryUseCase = new CategoryUseCase();
+    });
+
+    it("create new Category", async () => {
+        const newCategory = await categoryUseCase.create(category);
+        expect(newCategory).toMatchObject(category);
+    });
+});
