de6a5b014373a02bd2ae0f6dc1827bf8777ac8a4
BrunaBelo <anabruna28@gmail.com>
2021-08-15 18:02:43 -0300

    Add bidding use case

---
+++ src/use-cases/biddingUseCase.ts
@@ -0,0 +1,17 @@
+import { Bidding } from "../model/bidding";
+import { BiddingRepository } from "../repository/biddingRepository";
+
+class BiddingUseCase {
+    private repository: BiddingRepository;
+
+    constructor() {
+        this.repository = new BiddingRepository();
+    }
+
+    async create(bidding: Bidding): Promise<Bidding> {
+        const newBidding = await this.repository.create(bidding);
+        return newBidding;
+    }
+
+}
+export { BiddingUseCase };
