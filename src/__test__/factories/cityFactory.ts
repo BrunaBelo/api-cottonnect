bceaf93e46b8a1332d8a5b4877733eac8c4d420e
BrunaBelo <anabruna28@gmail.com>
2021-08-24 18:31:46 -0300

    Adjust unit city test to use new factory

--- src/__test__/factories/cityFactory.ts
+++ src/__test__/factories/cityFactory.ts
@@ -3,7 +3,7 @@ import { Factory } from "./typeorm-factory/Factory";
 import faker from 'faker/locale/pt_BR';
 
 export const cityCuritiba = new Factory(City)
-    .attr("id", faker.datatype.uuid())
-    .attr("name", "Curitiba")
-    .attr("ibge", 4106902)
-    .attr("state_id", "")
+  .attr("id", faker.datatype.uuid())
+  .attr("name", "Curitiba")
+  .attr("ibge", 4106902)
+  .attr("state_id", "")
--- src/__test__/unit/cityUnitTest.spec.ts
+++ src/__test__/unit/cityUnitTest.spec.ts
@@ -1,37 +1,32 @@
-import "../../database/connection";
 import connection from "../../database/connection";
 import { City } from "../../model/city";
-import { State } from "../../model/state";
 import { CityUseCase } from "../../use-cases/cityUseCase";
-import { StateUseCase } from "../../use-cases/stateUseCase";
 import { cityCuritiba } from "../factories/cityFactory";
 import { stateParana } from "../factories/stateFactory";
 
-let cityUseCase: CityUseCase;
-let stateUseCase: StateUseCase;
+describe("City", () => {
+  let cityUseCase: CityUseCase;
+  let city: City;
 
-let state: State
-const city = cityCuritiba
+  beforeAll(async () => {
+    await connection.create();
+    cityUseCase = new CityUseCase();
+    city = cityCuritiba.build();
+  });
 
-describe("Create City", () => {
-    beforeAll(async () => {
-        await connection.create();
-    });
+  afterAll(async () => {
+    await connection.close();
+  });
 
-    afterAll(async () => {
-        await connection.close();
-    });
+  beforeEach(async () => {
+    await connection.clear();
+  });
 
-    beforeEach(async () => {
-        await connection.clear();
-        cityUseCase = new CityUseCase();
-        stateUseCase = new StateUseCase();
-        state = await stateUseCase.create(stateParana);
-    });
-
+  describe("Create City", () => {
     it("create new City", async () => {
-        city.state_id = state.id
-        const newCity = await cityUseCase.create(city);
-        expect(newCity).toMatchObject(city);
+      city.state_id = (await stateParana.create()).id
+      const newCity = await cityUseCase.create(city);
+      expect(newCity).toMatchObject(city);
     });
+  });
 });
