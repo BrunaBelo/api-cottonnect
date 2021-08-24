7f53af87a8bb71cb5ceb3631e65587270c087eef
BrunaBelo <anabruna28@gmail.com>
2021-08-24 19:05:08 -0300

    Adjust unit donation test to use new factory

--- src/__test__/unit/donationObjectTest.spec.ts
+++ src/__test__/unit/donationObjectTest.spec.ts
@@ -1,26 +1,31 @@
 import connection from "../../database/connection";
+import { DonationObject } from "../../model/donationObject";
 import { DonationObjectUseCase } from "../../use-cases/donationObjectUseCase";
 import { donation01 } from "../factories/donationObjectFactory";
 
-let donationObjectUseCase: DonationObjectUseCase;
-const donation = donation01
+describe("Donation Object", () => {
+  let donationObjectUseCase: DonationObjectUseCase;
+  let donation: DonationObject;
 
-describe("Create Donation Object", () => {
-    beforeAll(async () => {
-        await connection.create();
-    });
+  beforeAll(async () => {
+    await connection.create();
+    donationObjectUseCase = new DonationObjectUseCase();
+    donation = donation01.build()
+  });
 
-    afterAll(async () => {
-        await connection.close();
-    });
+  afterAll(async () => {
+    await connection.close();
+  });
 
-    beforeEach(async () => {
-        await connection.clear();
-        donationObjectUseCase = new DonationObjectUseCase();
-    });
+  beforeEach(async () => {
+    await connection.clear();
+  });
 
+  describe("Create Donation Object", () => {
     it("create new Donation", async () => {
-        const newDonation = await donationObjectUseCase.create(donation);
-        expect(newDonation).toMatchObject(donation);
+      const newDonation = await donationObjectUseCase.create(donation);
+      expect(newDonation).toMatchObject(donation);
     });
+  });
 });
+
