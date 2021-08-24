a6714508838fe92834bd016b1cb61a8d0e39640b
BrunaBelo <anabruna28@gmail.com>
2021-08-24 19:42:08 -0300

    Add unit test to auction

--- src/__test__/unit/auctionUnitTest.spec.ts
+++ src/__test__/unit/auctionUnitTest.spec.ts
@@ -1,18 +1,38 @@
 import connection from "../../database/connection";
+import { Auction } from "../../model/auction";
+import { AuctionUseCase } from "../../use-cases/auctionUseCase";
+import { auction01 } from "../factories/auctionFactory";
+import { cityCuritiba } from "../factories/cityFactory";
+import { donation01 } from "../factories/donationObjectFactory";
+import { roleAdmin } from "../factories/roleFactory";
+import { stateParana } from "../factories/stateFactory";
+import { user01 } from "../factories/userFactory";
 
-describe("create auction", () => {
-    beforeAll(async () => {
-        await connection.create();
-    });
+describe("Auction", () => {
+  let auctionUseCase: AuctionUseCase;
+  let auction: Auction;
 
-    afterAll(async () => {
-        await connection.close();
-    });
+  beforeAll(async () => {
+    await connection.create();
+    auctionUseCase = new AuctionUseCase();
+  });
 
-    beforeEach(async () => {
-        await connection.clear();
-    });
+  afterAll(async () => {
+    await connection.close();
+  });
 
-    it("create auction", async () => {
+  beforeEach(async () => {
+    await connection.clear();
+    const city = await cityCuritiba.create({ state_id: (await stateParana.create()).id });
+    const user = await user01.create({ role_id: (await roleAdmin.create()).id, city_id: city.id });
+    const donation = await donation01.create();
+    auction = auction01.build({ donation_object_id: donation.id, user_id: user.id });
+  });
+
+  describe("Create auction", () => {
+    it("create new auction", async () => {
+      const newAuction = await auctionUseCase.create(auction);
+      expect(newAuction).toMatchObject(auction);
     });
+  });
 });
