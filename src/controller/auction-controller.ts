070cf127bf814706205ae57ada2687913528f716
BrunaBelo <anabruna28@gmail.com>
2021-11-17 20:38:47 -0300

    Fix indentation

--- src/controller/auction-controller.ts
+++ src/controller/auction-controller.ts
@@ -4,13 +4,13 @@ import { Auction } from "../model/auction";
 import { AuctionUseCase } from "../use-cases/auction-use-case";
 
 class AuctionController {
-    async create(request: Request, response: Response): Promise<Response> {
-        const { closingData, userId, donationObjectId }: Auction = request.body;
+  async create(request: Request, response: Response): Promise<Response> {
+    const { closingData, userId, donationObjectId }: Auction = request.body;
 
-        const AuctionUserCase = new AuctionUseCase();
+    const AuctionUserCase = new AuctionUseCase();
 
-        const newAuction = await AuctionUserCase.create({ closingData, userId, donationObjectId });
-        return response.status(201).json(newAuction);
-    }
+    const newAuction = await AuctionUserCase.create({ closingData, userId, donationObjectId });
+    return response.status(201).json(newAuction);
+  }
 }
 export { AuctionController };
