b41b2c0c557e1de7c33598b3dfbd284e9e4ae20b
BrunaBelo <anabruna28@gmail.com>
2022-03-22 20:02:38 -0300

    Add upload photos to cloudinary

---
+++ src/interfaces/IPhoto.ts
@@ -0,0 +1,5 @@
+export interface IPhoto {
+  url: string,
+  assetId: string,
+  publicId: string
+}
---
+++ src/service/cloudinary/savePhoto.ts
@@ -0,0 +1,46 @@
+import { Buffer } from "buffer";
+import axios from "axios";
+
+import { IPhoto } from "../../interfaces/IPhoto"
+
+interface IPhotoResponse {
+  data: {
+    asset_id: string,
+    url: string,
+    public_id: string
+  }
+}
+
+const savePhoto = async (photo: Express.Multer.File): Promise<IPhotoResponse> => {
+  const cloudName = process.env.CLOUDINARY_CLOUD
+  const url = process.env.CLOUDINARY_BASEURL.replace("CLOUD_NAME", cloudName)
+  const apiKey = process.env.CLOUDINARY_KEY
+  const presetName = process.env.CLOUDNARY_PRESET
+  let response
+
+  try {
+    response = await axios.post(url, {
+      file: `data:${photo.mimetype};base64,${photo.buffer.toString('base64')}`,
+      upload_preset: presetName,
+      timestamp: Math.floor(new Date().getTime() / 1000),
+      api_key: apiKey,
+    }) as IPhotoResponse
+  } catch (error) {
+    console.log(error)
+  }
+
+  return response
+}
+
+const savePhotos = async(photos: Express.Multer.File[]): Promise<IPhoto[]> => {
+  const result = []
+
+  for await (const photoToUpload of photos) {
+    const { data: { asset_id, public_id, url }} = await savePhoto(photoToUpload)
+    result.push({ assetId: asset_id, publicId: public_id, url })
+  }
+
+  return result
+}
+
+export default savePhotos
