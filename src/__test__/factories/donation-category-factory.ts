a39171731e9a9f2bc055c4cc74bcf6baae78831b
BrunaBelo <anabruna28@gmail.com>
2022-03-27 11:10:42 -0300

    Remove not necessary test

--- src/__test__/factories/donation-category-factory.ts
+++
@@ -1,8 +0,0 @@
-import { Factory } from "./typeorm-factory/factory";
-import faker from 'faker/locale/pt_BR';
-import { DonationCategory } from "../../model/donation-category";
-
-export const donationCategory01 = new Factory(DonationCategory)
-    .attr("id", faker.datatype.uuid())
-    .attr("donationObjectId", "")
-    .attr("donationCategoryId", "")
